e7f8dc5fca797300d675b5a1af5775df356968bc
Keith Davis <keith.davis@zunisoft.com>
2017-11-03 10:49:42 -0700

    Initial Commit

---
+++ Tank War Redux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tank War Redux
+//
+//  Created by Keith Davis on 11/3/17.
+//  Copyright © 2017 Keith Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
